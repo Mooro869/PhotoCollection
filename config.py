c71ba837b570c3e12d56a93e050d169e21d7ca31
MOORO <111493382+Mooro869@users.noreply.github.com>
2024-11-15 19:26:39 +0300

    Изменена структура проекта Неисправленная ошибка в Edit_Tag

--- config.py
+++ config.py
@@ -3,17 +3,33 @@ icon = 'files/icon.ico'
 
 # UI файлы
 main_window = 'files/main_window.ui'
-tags_window = 'files/tags_window.ui'
 
-new_album = 'files/new_album.ui'
-new_tag = 'files/new_tag.ui'
-del_tag = 'files/del_tag.ui'
-edit_tag = 'files/edit_tag.ui'
-all_tag = 'files/all_tags.ui'
 
-# Текста для statusbar
+# UI файлы АЛЬБОМЫ
+new_album = 'files/album/new_album.ui'
+
+del_album = 'files/album/del_album.ui'
+edit_album = 'files/album/edit_album.ui'
+del_image = 'files/album/del_image.ui'
+add_image = 'files/album/add_image.ui'
+
+all_img_album = 'files/album/all_img_album.ui'
+
+
+# UI файлы ТЕГИ
+new_tag = 'files/tags/new_tag.ui'
+del_tag = 'files/tags/del_tag.ui'
+edit_tag = 'files/tags/edit_tag.ui'
+
+# Статусы
 new_album_text = 'Новый альбом создан'
 del_album_text = 'Альбом удален'
+
+
+new_image_text = 'Новое изображение добавлено'
+del_image_text = 'Изображение удалено'
+
+
 new_tag_text = 'Новый тег создан'
 del_tag_text = 'Тег удален'
 edit_tag_text = 'Тег изменен'
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from time import sleep
 import sqlite3
-import config as cfg
+import config
 
 from PyQt6 import QtCore, QtWidgets, QtGui, uic
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QComboBox, QTableWidgetItem, QToolBar
@@ -11,8 +11,7 @@ from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLa
 class PhotoCollection(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi(cfg.main_window, self)
-        self.create_alb.clicked.connect(self.new_alb)
+        uic.loadUi(config.main_window, self)
 
         # Нужно для того чтобы была возможность иметь доступ к statusbar в каждом классе
         global status_bar
@@ -20,12 +19,16 @@ class PhotoCollection(QMainWindow):
         status_bar = self.statusbar
 
         # Обработка тегов из menuBar
-        self.create_tag.triggered.connect(self.cr_tag)
-        self.edit_tag.triggered.connect(self.ed_tag)
-        self.del_tag.triggered.connect(self.dl_tag)
-        self.all_tags.triggered.connect(self.all_tag)
+        self.create_tag_2.triggered.connect(self.cr_tag)
+        self.edit_tag_2.triggered.connect(self.ed_tag)
+        self.del_tag_2.triggered.connect(self.dl_tag)
 
-        self.edits_tags.triggered.connect(self.alls_tags)
+        # Обработка кнопок главного окна
+        self.create_alb.clicked.connect(self.new_alb)
+
+        # Обработка альбомов из menuBar
+
+
 
         # # Обработчик экспортов из menuBar
         # self.export_txt.triggered.connect(...)
@@ -35,30 +38,33 @@ class PhotoCollection(QMainWindow):
         # self.export_json.triggered.connect(...)
         # self.export_xlsx.triggered.connect(...)
 
-    def alls_tags(self):
-        self.tags_menu = Tags_Menu()
-        self.tags_menu.show()
 
+
+    '''
+    ТЕГИ
+    '''
 
     def cr_tag(self):  # Обработчик создания нового тега
         self.tag = New_Tag()
         self.tag.show()
 
     def ed_tag(self):  # Обработчик изменения тега
-        ...
+        self.edit_tag = Edit_Tag()
+        self.edit_tag.snow()
 
     def dl_tag(self):  # Обработчик удаления тега
         self.tag = Del_Tag()
         self.tag.show()
 
 
+    '''
+    АЛЬБОМЫ
+    '''
+
     def new_alb(self):  # Обработчик создания нового альбома
         self.alb = New_Album()
         self.alb.show()
 
-    def all_tag(self):  # Обработчик появления всех тегов
-        self.all_tg = All_Tags()
-        self.all_tg.show()
 
 
 
@@ -66,98 +72,84 @@ class PhotoCollection(QMainWindow):
 class New_Tag(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi(cfg.new_tag, self)
+        uic.loadUi(config.new_tag, self)
         self.new_tag_btn.clicked.connect(self.check_new_tag)
 
     def check_new_tag(self):
-        con = sqlite3.connect(cfg.bd_file)
+        con = sqlite3.connect(config.bd_file)
         cur = con.cursor()
-        cur.execute(f"INSERT INTO tags(title) VALUES({self.new_tag_text.text()})")
+        cur.execute(f"INSERT INTO tags(title) VALUES('{self.new_tag_text.text()}')")
         con.commit()
         con.close()
         self.close()
-        status_bar.showMessage(f'{cfg.new_tag_text}', 3_000)
+        status_bar.showMessage(f'{config.new_tag_text}', 3_000)
 
         '''
-        проверка на существование тэга
+        проверка на существование тега
         '''
 
 
 class Del_Tag(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi(cfg.del_tag, self)
+        uic.loadUi(config.del_tag, self)
         self.del_tag_btn.clicked.connect(self.check_del_tag)
 
 
     def check_del_tag(self):
-        con = sqlite3.connect(cfg.bd_file)
+        con = sqlite3.connect(config.bd_file)
         cur = con.cursor()
         cur.execute(f"DELETE FROM tags WHERE title = '{self.del_tag_text.text()}'")
         con.commit()
         con.close()
         self.close()
-        status_bar.showMessage(f'{cfg.del_tag_text}', 3_000)
+        status_bar.showMessage(f'{config.del_tag_text}', 3_000)
 
 
     '''
-    проверка на существования тэга
+    проверка на существования тега
     '''
 
 
 class New_Album(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi(cfg.new_album, self)
+        uic.loadUi(config.new_album, self)
         self.new_alb_btn.clicked.connect(self.check_new_album)
 
     def check_new_album(self):
-        con = sqlite3.connect(cfg.bd_file)
+        con = sqlite3.connect(config.bd_file)
         cur = con.cursor()
         cur.execute(f"INSERT INTO album(title) VALUES('{self.new_alb_text.text()}')")
         con.commit()
         con.close()
         self.close()
-        status_bar.showMessage(f'{cfg.new_album_text}', 3_000)
+        status_bar.showMessage(f'{config.new_album_text}', 3_000)
 
 
-class All_Tags(QWidget):
+
+class Edit_Tag(QWidget):
     def __init__(self):
         super().__init__()
-        self.win = uic.loadUi(cfg.all_tag, self)
-        self.InitUI()
+        uic.loadUi(config.edit_tag, self)
 
-    def InitUI(self):
-        self.win.snow()
-        # con = sqlite3.connect(cfg.bd_file)
-        # cur = con.cursor()
-        # result = cur.execute(f'SELECT tags.id, tags.title FROM tags').fetchall()
-        # self.tableWiget.setRowCount(len(result))
-        # self.tableWiget.setColumnCount(len(result[0]))
-        # for i, element in enumerate(result):
-        #     for j, value in enumerate(element):
-        #         self.tableWiget.setItem(i, j, QTableWidgetItem(str(value)))
-        # con.close()
+        self.pushButton.clicked.connect(self.ok)
 
-class Tags_Menu(QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi(cfg.tags_window, self)
-        #self.InitUI(self)
-
-        # self.con = sqlite3.connect(cfg.bd_file)
-        # cur = self.con.cursor()
-        # self.con.close()
+        con = sqlite3.connect(config.bd_file)
+        cur = con.cursor()
+        result = cur.execute('SELECT title FROM tags').fetchall()
+        self.comboBox.addItems([item[0] for item in result])
+        con.close()
 
 
-
-
+    def ok(self):
+        status_bar.showMessage(f'{config.edit_tag_text}', 3_000)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     # Установка иконки
-    icon = QtGui.QIcon(cfg.icon)
+    icon = QtGui.QIcon(config.icon)
     app.setWindowIcon(icon)
 
     ex = PhotoCollection()
